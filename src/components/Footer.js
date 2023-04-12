6000d9a93f674b31664aa8c77d7db861f4ba4250
RAMANIKRISHNANR <ramanikrishnan.r26@gmail.com>
2023-04-12 08:11:49 +0530

    new

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+import Logo from '../assets/images/Logo.png';
+
+const Footer = () => (
+  // eslint-disable-next-line
+  <Box mt="80px" bgcolor="#FFF3F4">
+    <Stack gap="40px" sx={{ alignItems: 'center' }} flexWrap="wrap" px="40px" pt="24px">
+      <img src={Logo} alt="" style={{ width: '50px', height: '41px' }} />
+      <h1 sx={{ fontSize: { lg: '28px', xs: '20px' } }} mt="41px" textAlign="center" pb="4px">MultiFit Gym</h1>
+    </Stack>
+    <Typography variant="h5" sx={{ fontSize: { lg: '28px', xs: '20px' } }} mt="41px" textAlign="center" pb="40px">Made with ❤️ by krish</Typography>
+  </Box>
+);
+
+export default Footer;
